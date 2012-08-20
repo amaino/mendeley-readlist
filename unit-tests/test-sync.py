a1491ba3fdd4d3fb21fd0a84ae43ff53378ba47d
Nicolas Esteves <nicolas.esteves@mendeley.com>
2012-08-20 12:19:35 +0100

    Added more tests and improved handling of conflicts
    in synced_client.py
     * Added to_json to convert SyncedObject back to json, with a whitelist of fields for SyncedDocument
     * Added comments and cleanup of the (Simple)ConflictResolver classes
     * Changed "server" to "remote" to make it more consistent, cleanup in variable names
     * Added code to deal with document deleted on server but updated locally to recreate the document in needed
    
    test-sync.py
     * Added more unit tests
     * Changed the clear_library() and seed_library() to work in parallel to speed up the tests

--- unit-tests/test-sync.py
+++ unit-tests/test-sync.py
@@ -1,3 +1,4 @@
+from multiprocessing import Pool
 import os
 import sys
 import unittest
@@ -7,12 +8,30 @@ parent_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),"..")
 os.sys.path.insert(0, parent_dir) 
 from synced_client import *
 
-
-
 class TestEnv:
     sclient = None
     debug = True 
+    sleep_time = 1
     log_file = open("test-sync.log","w")
+
+    @staticmethod
+    def clear_library():
+        p = Pool()
+        p.map(remove_document, TestEnv.sclient.client.library()["document_ids"])
+
+    @staticmethod
+    def seed_library(count):
+        p = Pool()
+        ids = p.map(create_test_doc, range(count))
+        assert len(ids) == count
+        return ids
+
+def create_test_doc(index):
+    response = TestEnv.sclient.client.create_document(document={"type":"Book", "title":"Title %d"%index})
+    return response["document_id"]
+
+def remove_document(document_id):
+    assert ("error" not in TestEnv.sclient.client.delete_library_document(document_id))
 
 class TestDocumentsSyncing(unittest.TestCase):
 
@@ -22,27 +41,15 @@ class TestDocumentsSyncing(unittest.TestCase):
             TestEnv.log_file.write("#"+"-"*len(message)+"\n\n")
             TestEnv.sclient.dump_status(TestEnv.log_file)
 
-    def clear_library(self):
-        for doc in TestEnv.sclient.client.library()["document_ids"]:
-            TestEnv.sclient.client.delete_library_document(doc)
-
-    def seed_library(self, count):
-        ids = []
-        for i in range(count):
-            response = TestEnv.sclient.client.create_document(document={"type":"Book", "title":"Title %d"%i})
-            ids.append(response["document_id"])
-        self.assertEqual(len(ids), count)
-        return ids
-
     def document_exists(self, document_id):
         return "error" not in TestEnv.sclient.client.document_details(document_id)
       
     def setUp(self):
-        self.clear_library()
-
+        TestEnv.clear_library()
+    
     def test_fetch(self):
         count = 5
-        ids = self.seed_library(count)
+        ids = TestEnv.seed_library(count)
         
         # sync, should have count documents with matching ids
         TestEnv.sclient.sync()
@@ -52,10 +59,10 @@ class TestDocumentsSyncing(unittest.TestCase):
         # the status of all documents should be synced
         for document in TestEnv.sclient.documents.values():
             self.assertTrue(document.is_synced())
-
+    
     def test_local_delete(self):
         count = 5
-        ids = self.seed_library(count)
+        ids = TestEnv.seed_library(count)
         TestEnv.sclient.sync()
 
         # locally delete 1 document
@@ -95,10 +102,11 @@ class TestDocumentsSyncing(unittest.TestCase):
                 self.assertFalse(self.document_exists(doc_id))
             else:
                 self.assertTrue(self.document_exists(doc_id))
-        
+                
+    
     def test_server_delete(self):
         count = 5 
-        ids = self.seed_library(count)
+        ids = TestEnv.seed_library(count)
         TestEnv.sclient.sync()
 
         # delete one doc on the server
@@ -113,25 +121,137 @@ class TestDocumentsSyncing(unittest.TestCase):
         for doc_id in ids[1:]:
             self.assertTrue(doc_id in TestEnv.sclient.documents.keys())
             self.assertTrue(TestEnv.sclient.documents[doc_id].is_synced())
+
+    def test_local_update_remote_delete(self):
+        new_local_title = "updated_local_title"
+        count = 5 
+        ids = TestEnv.seed_library(count)
+        TestEnv.sclient.sync()  
+
+        local_document = TestEnv.sclient.documents[ids[0]]
+        original_version = local_document.version()
+
+        # do a remote delete
+        response = TestEnv.sclient.client.delete_library_document(ids[0])
+        self.assertFalse(self.document_exists(ids[0]))
+        self.assertTrue(local_document.is_synced())
+
+        # do a local update
+        local_document.update({"title":new_local_title})
+        self.assertTrue(local_document.is_modified())
+
+        TestEnv.sclient.sync()
         
+        # the default conflict resolver should recreate the local version
+        self.assertTrue(local_document.is_synced())
+        
+        # the "new" document should have a different id and version
+        self.assertNotEqual(local_document.id(), ids[0])
+        self.assertNotEqual(local_document.version(), original_version)    
+        self.assertFalse(self.document_exists(ids[0]))  
+        self.assertEqual(len(TestEnv.sclient.documents), count)
 
-    # def test_nop(self):
-    #     pass
+    def test_local_delete_remote_update(self):
+        new_remote_title = "updated_remote_title"
+        count = 5 
+        ids = TestEnv.seed_library(count)
+        TestEnv.sclient.sync()  
 
-    # def test_local_update_remote_delete(self):
-    #     pass
+        local_document = TestEnv.sclient.documents[ids[0]]
+        original_version = local_document.version()
 
-    # def test_local_update_remote_update_conflict(self):
-    #     pass
+        # do a remote update
+        time.sleep(TestEnv.sleep_time)
+        response = TestEnv.sclient.client.update_document(ids[0], document={"title":new_remote_title})
+        remote_version = response["version"]
+        self.assertTrue("error" not in response)
+        self.assertTrue(remote_version > original_version)
+        self.assertTrue(local_document.is_synced())
 
+        # delete the local document
+        local_document.delete()
+        self.assertTrue(local_document.is_deleted())
+
+        TestEnv.sclient.sync()
+        
+        # the default conflict resolver should keep the server version if more recent
+        self.assertTrue(local_document.is_synced())
+        self.assertEqual(local_document.version(), remote_version)
+
+        # check that the document is still on the server
+        self.assertTrue(self.document_exists(ids[0]))
+    
     def test_local_update_remote_update_no_conflict(self):
-        pass
+        # update different fields locally and remotely
+        new_remote_title = "updated_remote_title"
+        new_local_year = 1997
 
+        count = 5 
+        ids = TestEnv.seed_library(count)
+        TestEnv.sclient.sync()  
+
+        local_document = TestEnv.sclient.documents[ids[0]]
+        original_version = local_document.version()
+        
+        # do a remote update
+        time.sleep(TestEnv.sleep_time)
+        response = TestEnv.sclient.client.update_document(ids[0], document={"title":new_remote_title})
+        self.assertTrue("error" not in response)
+        self.assertTrue(response["version"] > original_version)
+        self.assertTrue(local_document.is_synced())
+
+        # do a local update
+        local_document.update({"year":new_local_year})
+        self.assertTrue(local_document.is_modified())  
+
+        self.log_status("before sync")
+        TestEnv.sclient.sync()
+        self.log_status("after sync")
+
+        # no conflict so both fields should be updated
+        self.assertTrue(local_document.is_synced())
+        self.assertEqual(local_document.object.title, new_remote_title)
+        self.assertEqual(local_document.object.year, new_local_year)
+
+    
+    def test_local_update_remote_update_conflict(self):
+        # update the same field locally and remotely to force a conflict
+        new_remote_title = "updated_remote_title"
+        new_local_title = "updated_local_title"
+
+        count = 5 
+        ids = TestEnv.seed_library(count)
+        TestEnv.sclient.sync()  
+
+        local_document = TestEnv.sclient.documents[ids[0]]
+        original_version = local_document.version()
+        
+        # do a remote update
+        time.sleep(TestEnv.sleep_time)
+        response = TestEnv.sclient.client.update_document(ids[0], document={"title":new_remote_title})
+        self.assertTrue("error" not in response)
+        self.assertTrue(response["version"] > original_version)
+        self.assertTrue(local_document.is_synced())
+
+        # do a local update
+        local_document.update({"title":new_local_title})
+        self.assertTrue(local_document.is_modified())  
+
+        self.log_status("before sync")
+        TestEnv.sclient.sync()
+        self.log_status("after sync")
+
+        # default conflict resolver will choose the local version
+        # TODO test different strategies
+        self.assertTrue(local_document.is_synced())
+        self.assertEqual(local_document.object.title, new_local_title)
+
+    
     def test_local_update(self):
         new_title = "updated_title"
 
         count = 5 
-        ids = self.seed_library(count)
+        ids = TestEnv.seed_library(count)
         TestEnv.sclient.sync()     
 
         # change the title of one document
@@ -142,6 +262,7 @@ class TestDocumentsSyncing(unittest.TestCase):
         
         # the document should be marked as modified
         self.assertTrue(local_document.is_modified())
+        self.assertEqual(local_document.version(), original_version)
         for doc_id in ids[1:]:
             self.assertTrue(TestEnv.sclient.documents[doc_id].is_synced())
         
@@ -161,11 +282,12 @@ class TestDocumentsSyncing(unittest.TestCase):
         self.assertEqual(details["title"], new_title)
         self.assertEqual(details["version"], local_document.version())
         
+    
     def test_remote_update(self):
         new_title = "updated_title"
 
         count = 5 
-        ids = self.seed_library(count)
+        ids = TestEnv.seed_library(count)
         TestEnv.sclient.sync()        
         
         local_document = TestEnv.sclient.documents[ids[0]]
